adbe4b70c68e8d2a53b517457d75d17415ed5960
cubes1 <cubes1@cubes1-Lenovo-E50-80>
2016-08-09 16:32:24 +0200

    avgust 9 pre

---
+++ application/controllers/ParagvajController.php
@@ -0,0 +1,15 @@
+<?php
+
+    class ParagvajController extends Zend_Controller_Action {
+        
+        public function indexAction() {
+            
+        }
+        
+        public function postcodefinderAction() {
+            
+        }
+        
+        
+        
+    }
