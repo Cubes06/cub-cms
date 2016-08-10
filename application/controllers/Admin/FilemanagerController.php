8a7d146882ce6776eb4335fc4f746b20a0d0838b
Cubes php <phpcubes16@gmail.com>
2016-08-10 20:08:16 +0200

    avgust 10 vece

---
+++ application/controllers/Admin/FilemanagerController.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Admin_FilemanagerController extends Zend_Controller_Action {
+        
+        public function indexAction() {
+            
+            
+            
+        }//endf
+        
+        
+        public function connectorAction() {
+
+            
+            
+        }//endf
+        
+        
+        public function popupAction() {
+
+            
+            
+        }//endf
+        
+    } // end of class: Admin_FilemanagerController
